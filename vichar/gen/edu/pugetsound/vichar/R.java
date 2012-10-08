c68f6a2bffbe8e0ae081fc310450087d2996446d
Michael DuBois <michael@michael-dubois.com>
2012-10-08 16:20:30 -0700

    Created placeholder fragment with teapot bg

--- vichar/gen/edu/pugetsound/vichar/R.java
+++ vichar/gen/edu/pugetsound/vichar/R.java
@@ -13,14 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int teapot=0x7f020002;
     }
     public static final class id {
-        public static final int enter_game=0x7f070001;
-        public static final int menu_settings=0x7f070000;
+        public static final int augmented_reality_fragment=0x7f070000;
+        public static final int enter_game=0x7f070002;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int fragment_placeholder=0x7f030002;
     }
     public static final class menu {
         public static final int activity_game=0x7f060000;
