9cc5b5c9050f5d44ff76289bfe3bb58f83f73f08
Nathan Pastor <npastor@pugetsound.edu>
2012-10-08 23:19:21 -0700

    Initial work on saving/loading username to persistent storage

--- vichar/gen/edu/pugetsound/vichar/R.java
+++ vichar/gen/edu/pugetsound/vichar/R.java
@@ -15,8 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int enter_game=0x7f070001;
-        public static final int menu_settings=0x7f070000;
+        public static final int enter_game=0x7f070005;
+        public static final int enter_twitter_username=0x7f070000;
+        public static final int enter_vichar_username=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int ok_tw_username=0x7f070001;
+        public static final int ok_vichar_username=0x7f070003;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -28,11 +32,17 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_ok=0x7f04000b;
         public static final int enter_game=0x7f040004;
+        public static final int enter_tw_username=0x7f040009;
+        public static final int enter_vichar_username=0x7f04000a;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int test_activity_game=0x7f040005;
         public static final int title_activity_main=0x7f040003;
+        public static final int twitter_username_key=0x7f040006;
+        public static final int username_prefs=0x7f040008;
+        public static final int vichar_username_key=0x7f040007;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- vichar/src/edu/pugetsound/vichar/MainActivity.java
+++ vichar/src/edu/pugetsound/vichar/MainActivity.java
@@ -1,10 +1,14 @@
 package edu.pugetsound.vichar;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -31,4 +35,73 @@ public class MainActivity extends Activity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    
+    /**
+     * saves username to persistent storage
+     * @param user
+     * @param loginType
+     */
+    private void saveUsername(String user, boolean loginType)
+    {
+    	Log.d("viCHar","save username commenced");
+   		SharedPreferences sharedPref = getSharedPreferences(getString(R.string.username_prefs), 0);
+   		SharedPreferences.Editor editor = sharedPref.edit();
+   		
+    	if(loginType==false) //if this is a Twitter username
+    	{    		
+    		editor.putString(getString(R.string.twitter_username_key), user);
+    		editor.commit();
+    	}
+    	else  //if this is a ViChar server username
+    	{
+    		editor.putString(getString(R.string.vichar_username_key), user);
+    		editor.commit();
+    	}
+    	Log.d("viCHar","commited!");
+    	
+    	checkSave(loginType);
+    }
+    
+    /*
+     * Test method, return saved values
+     */
+    private String checkSave(boolean loginType)
+    {
+    	String toReturn;
+   		SharedPreferences sharedPref = getSharedPreferences(getString(R.string.username_prefs), 0);
+   		if (loginType==false)
+   		{
+   			toReturn = sharedPref.getString(getString(R.string.twitter_username_key), "Fail");
+   		}
+   		else
+   		{
+   			toReturn = sharedPref.getString(getString(R.string.vichar_username_key), "Fail");
+   		}
+   		Log.d("viCHar", toReturn);
+   		return toReturn;
+    }
+    
+    /**
+     * Called when either ok button pressed
+     * @param view
+     */
+    public void sendMessage(View view) 
+    {
+    	Log.d("viCHar","send message commenced");
+
+    	if(view.getId()==R.id.ok_tw_username)
+    	{
+    		EditText editText = (EditText) findViewById(R.id.enter_twitter_username);
+    		String message = editText.getText().toString();
+    		saveUsername(message, false);    		
+    	}
+    	else
+    	{
+    		EditText editText = (EditText) findViewById(R.id.enter_vichar_username);
+    		String message = editText.getText().toString();
+    		saveUsername(message, true);
+    	}  	
+    }
+    
+
 }
