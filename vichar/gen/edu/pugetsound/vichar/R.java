7464ca3c5f299ca624b4f77e26df3df9efbc313c
Nathan Pastor <npastor@pugetsound.edu>
2012-10-18 23:42:58 -0700

    All OAuth http activity now running in non-UI worker thread per Android requirements

--- vichar/gen/edu/pugetsound/vichar/R.java
+++ vichar/gen/edu/pugetsound/vichar/R.java
@@ -38,28 +38,30 @@ public final class R {
         public static final int activity_twitter_oauth_pin_dialog=0x7f060003;
     }
     public static final class string {
-        public static final int access_token_key=0x7f040014;
-        public static final int access_token_secret_key=0x7f040015;
+        /**  Preference Keys 
+         */
+        public static final int access_token_key=0x7f040012;
+        public static final int access_token_secret_key=0x7f040013;
         public static final int app_name=0x7f040000;
-        public static final int button_ok=0x7f04000b;
+        public static final int button_ok=0x7f040009;
         public static final int enter_game=0x7f040004;
-        public static final int enter_pin=0x7f040011;
-        public static final int enter_tw_username=0x7f040009;
-        public static final int enter_vichar_username=0x7f04000a;
+        public static final int enter_pin=0x7f04000f;
+        public static final int enter_tw_username=0x7f040007;
+        public static final int enter_vichar_username=0x7f040008;
         public static final int hello_world=0x7f040001;
-        public static final int login_with_twitter=0x7f040012;
-        public static final int login_with_vichar=0x7f040013;
+        public static final int login_with_twitter=0x7f040010;
+        public static final int login_with_vichar=0x7f040011;
         public static final int menu_settings=0x7f040002;
-        public static final int oauth_access_token=0x7f04000f;
-        public static final int oauth_callback_url=0x7f04000e;
-        public static final int oauth_consumer_key=0x7f04000c;
-        public static final int oauth_consumer_secret=0x7f04000d;
-        public static final int oauth_token_secret=0x7f040010;
-        public static final int prefs_key=0x7f040008;
+        public static final int oauth_access_token=0x7f04000d;
+        public static final int oauth_callback_url=0x7f04000c;
+        public static final int oauth_consumer_key=0x7f04000a;
+        public static final int oauth_consumer_secret=0x7f04000b;
+        public static final int oauth_token_secret=0x7f04000e;
+        public static final int prefs_key=0x7f040006;
         public static final int test_activity_game=0x7f040005;
         public static final int title_activity_main=0x7f040003;
-        public static final int twitter_username_key=0x7f040006;
-        public static final int vichar_username_key=0x7f040007;
+        public static final int twitter_username_key=0x7f040014;
+        public static final int vichar_username_key=0x7f040015;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- vichar/src/edu/pugetsound/vichar/TwitterOAuthActivity.java
+++ vichar/src/edu/pugetsound/vichar/TwitterOAuthActivity.java
@@ -5,6 +5,7 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -25,6 +26,7 @@ public class TwitterOAuthActivity extends Activity {
 	WebView webView;
 	String  oAuthUrl;
 	RequestToken requestToken;
+	AccessToken accessToken;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,7 +45,7 @@ public class TwitterOAuthActivity extends Activity {
         Log.d("ViChar", "Activity created, calling authorizeTwitter");
         authorizeTwitter();
     }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_twitter_oauth, menu);
@@ -58,64 +60,24 @@ public class TwitterOAuthActivity extends Activity {
 		twitter = new TwitterFactory().getInstance();
 		twitter.setOAuthConsumer(getString(R.string.oauth_consumer_key), getString(R.string.oauth_consumer_secret));
     	Log.d("ViChar", "twitter initialized");
-		try
-		{
-			requestToken = twitter.getOAuthRequestToken();
-			Log.d("ViChar", "request token retrieved");
-			oAuthUrl = requestToken.getAuthorizationURL();
-			Log.d("ViChar", "url retrieved");
-			//WebView webview = (WebView) findViewById(R.id.twitter_auth_webview);
-			Log.d("Vichar", oAuthUrl);
-			System.out.println(this.webView);
-			this.webView.loadUrl(oAuthUrl);
-		}
-		catch (TwitterException te)
-		{
-			System.out.println(te.getStatusCode());
-			if(401 == te.getStatusCode())
-	        {
-	        	System.out.println("Unable to get the request token.");
-	        } else{
-	        	te.printStackTrace();
-	        }
-		}
-		catch (IllegalStateException ex)
-		{
-			System.out.println(ex);
-		}
+    	//call private class to retrieve request token, true passed as a dummy variable
+    	new RetrieveRequestToken().execute(true);
 	}
     
     /**
      * Parses authorization pin entered by user, and finishes Twitter
-     * authorization process by retrieving Oauth access token
+     * authorization process by retrieving OAuth access token
      * @param view The calling view
      */
     public void sendPin(View view)
     {
+    	//get pin entered by user
     	EditText editText = (EditText) findViewById(R.id.twitter_auth_pin);
 		String pin = editText.getText().toString();
-		System.out.println(pin);		
-		AccessToken accessToken = null;
-		try{
-	         if(pin.length() > 0)
-	         {
-	        	 accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-	         }else
-	         {
-	        	 accessToken = twitter.getOAuthAccessToken();
-	         }
-	         System.out.println("pin received...now saving accessToken");
-	         storeAccessToken(accessToken);
-	    } 
-		catch (TwitterException te) 
-		{
-	        if(401 == te.getStatusCode()){
-	          System.out.println("Unable to get the access token.");
-	        }else{
-	          te.printStackTrace();
-	        }
-		}
-    }
+		System.out.println(requestToken);
+		//call private class to retrieve access token, using request token and pin
+		new RetrieveAccessToken().execute(pin);		
+	}
     
     /**
      * Saves access token to persistent storage, so that user information
@@ -127,5 +89,109 @@ public class TwitterOAuthActivity extends Activity {
     	PreferenceUtility prefs = new PreferenceUtility();
     	prefs.saveString(getString(R.string.access_token_key), accessToken.getToken(), this);
     	prefs.saveString(getString(R.string.access_token_secret_key), (String)accessToken.getTokenSecret(), this);
+    }
+    
+    /**
+     * Retrieves OAuth request token in seperate thread (dummy paramters)
+     * @author Nathan P
+     * @version 10/18/12
+     */
+    private class RetrieveRequestToken extends AsyncTask<Boolean, Boolean, Boolean> 
+    {    	
+    	/**
+    	 * Retrieve request token
+    	 * @param Dummy parameter, has no purpose
+    	 * @return True if successful, false if not
+    	 */
+    	@Override
+        protected Boolean doInBackground(Boolean...booleans)
+        {
+            boolean result = false;
+            try 
+            {
+            	requestToken = twitter.getOAuthRequestToken();
+            	result = true;
+            }
+            catch (TwitterException te)
+        	{
+    			System.out.println(te.getStatusCode());
+    			if(401 == te.getStatusCode())
+    	        {
+    	        	System.out.println("Unable to get the request token.");
+    	        } else{
+    	        	te.printStackTrace();
+    	        }
+    		}
+    		catch (IllegalStateException ex)
+    		{
+    			System.out.println(ex);
+    		}
+            return result;
+        }
+    	
+        /**
+         * Displays results by setting URL in webview
+         */
+        @Override
+        protected void onPostExecute(Boolean result) 
+        {
+        	oAuthUrl = requestToken.getAuthorizationURL();
+			Log.d("ViChar", "url retrieved");
+			Log.d("Vichar", oAuthUrl);
+			webView.loadUrl(oAuthUrl);
+        }
+    }
+    
+    /**
+     * Retrieves OAuth access token in non-UI worker thread
+     * @author Nathan P
+     * @version 10/18/12
+     */
+    private class RetrieveAccessToken extends AsyncTask<String, Boolean, Boolean>
+    {
+    	/**
+    	 * Retrieves access token
+    	 * @param Array of Strings, contain OAuth pin entered by user
+    	 * @return True if successful, false if not
+    	 */
+    	@Override
+        protected Boolean doInBackground(String...pins)
+        {    		
+    		String pin = pins[0];
+    		System.out.println(pin + "pin in worker thread");
+            boolean result = false;
+            try{
+            	if(pin.length() > 0)
+            	{
+            		accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+            		System.out.println(accessToken + "if");
+            	}
+            	else
+            	{
+            		accessToken = twitter.getOAuthAccessToken();
+            		System.out.println(accessToken + "else");
+            	}
+            	result = true;
+            	storeAccessToken(accessToken);
+            } 
+            catch (TwitterException te) 
+            {
+            	if(401 == te.getStatusCode()){
+            		System.out.println("Unable to get the access token.");
+            	}else{
+            		te.printStackTrace();
+            	}
+            }
+            return result;
+        }
+    	
+        /**
+         * No UI to update here
+         */
+        @Override
+        protected void onPostExecute(Boolean result) 
+        {
+        	return;
+        }
     }
 }
