a55a1517793a3e11095ab234ffb450bdcd04302f
Nathan Pastor <npastor@pugetsound.edu>
2012-10-23 15:15:35 -0700

    Now (hopefully) posting tweets via worker thread

--- vichar/src/edu/pugetsound/vichar/MainMenuActivity.java
+++ vichar/src/edu/pugetsound/vichar/MainMenuActivity.java
@@ -7,10 +7,6 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Context;
 
--- vichar/src/edu/pugetsound/vichar/TweetActivity.java
+++ vichar/src/edu/pugetsound/vichar/TweetActivity.java
@@ -1,21 +1,33 @@
 package edu.pugetsound.vichar;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.support.v4.app.*;
 
 public class TweetActivity extends Fragment {
 
+	//current Twitter challenge prompt
 	private String curPrompt;
+	//current Twitter post attempt
+	private int postAttempt;
+	//max post attempts
+	private final int POST_ATTEMPT_LIMIT = 3;
 	
     @Override
     public View onCreateView(LayoutInflater inflater, 
     							ViewGroup container, Bundle savedInstanceState) {
         //get default prompt text
     	curPrompt = getString(R.string.default_twitter_prompt);
+    	postAttempt = 0;
     	//set default prompt text
     	TextView twitterPrompt = (TextView) getView().findViewById(R.id.cur_twitter_challenge);
     	twitterPrompt.setText(curPrompt);
@@ -33,5 +45,66 @@ public class TweetActivity extends Fragment {
     	//update display
     	TextView twitterPrompt = (TextView) getView().findViewById(R.id.cur_twitter_challenge);
     	twitterPrompt.setText(curPrompt);
+    }
+    
+    /**
+     * Sends tweet
+     * @param view
+     */
+    public void sendTweet(View view)
+    {
+    	//retrieve access token and access secret from memory
+    	PreferenceUtility prefs = new PreferenceUtility();
+    	String accessToken = prefs.returnSavedString(getString(R.string.access_token_key), 
+    													getString(R.string.prefs_error), this.getActivity());
+    	String accessSecret = prefs.returnSavedString(getString(R.string.access_token_secret_key), 
+														getString(R.string.prefs_error), this.getActivity()); 
+        //initialize twitter
+    	ConfigurationBuilder cb = new ConfigurationBuilder();
+    	cb.setOAuthConsumerKey(getString(R.string.oauth_consumer_key))
+    	  .setOAuthConsumerSecret(getString(R.string.oauth_consumer_secret))
+    	  .setOAuthAccessToken(accessToken)
+    	  .setOAuthAccessTokenSecret(accessSecret);
+    	TwitterFactory tf = new TwitterFactory(cb.build());
+    	Twitter twitter = tf.getInstance();
+    	//pass twitter object to worker thread
+    	new PostTweet().execute(twitter);
+    }
+    	
+   
+    /**
+     * Posts tweets
+     * @author Nathan P
+     * @version 10/23/12
+     */
+    private class PostTweet extends AsyncTask<Twitter, Boolean, Boolean>   {
+    	
+    	/**
+    	 * Posts tweet via worker thread
+    	 */
+     	@Override
+        protected Boolean doInBackground(Twitter...twitters)   {
+     		boolean result = false;  
+     		//get twitter object
+     		Twitter twitter = twitters[0];
+     		//get text of tweet
+     		EditText tweetPane = (EditText) getView().findViewById(R.id.tweet_pane);
+     		String tweet = tweetPane.getText().toString();
+        	//execute the post!
+            try {
+            	twitter.updateStatus(tweet);
+            	result = true;
+            }
+            catch (TwitterException te)  {
+            	//attempt maximum of POST_ATTEMPT_LIMIT times on failure
+    			if(postAttempt<POST_ATTEMPT_LIMIT)  {
+    				postAttempt++;
+    				new PostTweet().execute(twitter);
+    			} else {
+    				postAttempt=0;
+    			}
+    		}
+            return result;
+     	}
     }
 }
