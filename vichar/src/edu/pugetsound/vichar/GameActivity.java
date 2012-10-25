bb0ec3b24e3b287ccd0dbcadfd2fe09004f5c151
Michael DuBois <michael@michael-dubois.com>
2012-10-25 11:57:09 -0700

    Touch dx and dy with httpService bound

--- vichar/src/edu/pugetsound/vichar/GameActivity.java
+++ vichar/src/edu/pugetsound/vichar/GameActivity.java
@@ -1,5 +1,11 @@
 package edu.pugetsound.vichar;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.pugetsound.vichar.SocketService.LocalBinder;
 import android.content.ComponentName;
 import android.content.Context;
@@ -25,7 +31,10 @@ public class GameActivity extends FragmentActivity implements OnTouchListener {
 
 	private View gameView;
 	private SocketService socketService;
+	private HttpService httpService;
     boolean isBoundToSocketService = false;
+    boolean isBoundToHttpService = false;
+    private float touchX, touchY;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,10 @@ public class GameActivity extends FragmentActivity implements OnTouchListener {
         
     	doBindSocketService();
     	Intent intent = new Intent(GameActivity.this, SocketService.class);
+    	startService(intent);
+    	
+    	doBindHttpService();
+    	intent = new Intent(GameActivity.this, HttpService.class);
     	startService(intent);
         /*// Bind this activity to Networking Service
         Intent intent = new Intent(GameActivity.this, HttpService.class);
@@ -77,23 +90,62 @@ public class GameActivity extends FragmentActivity implements OnTouchListener {
         	//return true; // true indicates event is consumed
         //}
     	if(v.equals(this.gameView)) {
+    		
+    		float dx = 0f;
+    		float dy = 0f;
+    		
+    		if(ev.getAction() == MotionEvent.ACTION_MOVE) {
+    			dx = ev.getX() - touchX;
+    			dy = ev.getY() - touchY;
+    			Log.d(this.toString(),"Touch move");
+    		}
+    		if(ev.getAction() == MotionEvent.ACTION_DOWN 
+    				|| ev.getAction() == MotionEvent.ACTION_MOVE) {
+    			// Remember new touch coors
+    			touchX = ev.getX();
+    			touchY = ev.getY();
+    			Log.d(this.toString(),"Touch down or move");
+    		} else if(ev.getAction() == MotionEvent.ACTION_UP) {
+    			// reset values
+    			touchX = 0f;
+    			touchY = 0f;
+    			dx = 0f;
+    			dy = 0f;
+    			Log.d(this.toString(),"Touch up");
+    		}
+    		
     		// Touch propagated to gameView.
             float x = ev.getX();
             float y = ev.getY();
             Log.d(this.toString(),"Touch event propagated to gameView");
 	    	Log.d(this.toString(),"startX: " + x);
 	    	Log.d(this.toString(),"startY: " + y);
+	    	Log.d(this.toString(),"dX: " + dx);
+	    	Log.d(this.toString(),"dY: " + dy);
+	    	
+	    	try {
+		    	JSONObject json = new JSONObject("{\"turret\":{\"position\":\"100,0,300\",\"ID\":\"1\"}}");
+		    	JSONObject turret = json.getJSONObject("turret");
+		    	String position = json.getString("position");
+		    	Log.d(this.toString(),position);
+		    	List<String> coors = Arrays.asList(position.split("\\s*,\\s*"));
+		    	coors.add(0, coors.get(0) + dx);
+		    	coors.add(1, coors.get(1) + dy);
+		    	turret.put("position", "");
+		    	Log.d(this.toString(),turret.toString());
+		    	json.put("turret", turret);
+		    	Log.d(this.toString(),json.toString());
+		    	//httpService.send(json);
+	    	} catch (JSONException e) {
+	    		//something
+	    		Log.i(this.toString(),"JSONException");
+	    	}
 	    	
 	    	// Update server
-           /* Intent intent = new Intent(GameActivity.this, HttpService.class);
-            intent.putExtra("touchX", x);
-            intent.putExtra("touchY", y);
-            intent.putExtra("touchTimestamp", ev.getDownTime());
-            httpService.send(intent);*/
 	    	//socketService.readJSON();
-	    	socketService.postTest();
+	    	//socketService.postTest();
     	}
-        return false; //false indicates the event is not consumed
+        return true; //false indicates the event is not consumed
     }
     
     
@@ -127,7 +179,7 @@ public class GameActivity extends FragmentActivity implements OnTouchListener {
         				IBinder service)
         {
         	Log.d(this.toString(),"Start onServiceConnected");
-	        LocalBinder binder = (LocalBinder) service;
+        	SocketService.LocalBinder binder = (SocketService.LocalBinder) service;
 	        socketService = binder.getService();
 	        isBoundToSocketService = true;
 	        Log.d(this.toString(),"Bound to SocketSevice");
@@ -152,6 +204,41 @@ public class GameActivity extends FragmentActivity implements OnTouchListener {
             // Detach our existing connection.
             unbindService(socketServiceConnection);
             isBoundToSocketService = false;
+        }
+    }
+    
+    private ServiceConnection httpServiceConnection = new ServiceConnection() 
+    { //need this to create the connection to the service
+        public void onServiceConnected(
+        				ComponentName className, 
+        				IBinder service)
+        {
+        	Log.d(this.toString(),"Start onServiceConnected");
+        	HttpService.LocalBinder binder = (HttpService.LocalBinder) service;
+	        httpService = binder.getService();
+	        isBoundToHttpService = true;
+	        Log.d(this.toString(),"Bound to HttpSevice");
+        }
+
+        public void onServiceDisconnected(ComponentName className) 
+        {
+        	isBoundToHttpService = false;
+        }
+    };
+    
+    private void doBindHttpService() {
+    	Log.d(this.toString(),"Binding HttpSevice");
+        bindService(new Intent(GameActivity.this, HttpService.class), 
+        		httpServiceConnection, Context.BIND_AUTO_CREATE);
+        isBoundToHttpService = true;
+    }
+
+
+    private void doUnbindHttpService() {
+        if (isBoundToSocketService) {
+            // Detach our existing connection.
+            unbindService(httpServiceConnection);
+            isBoundToHttpService = false;
         }
     }
     
