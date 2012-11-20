ea4bc92524f1df71e7446d0608d458e7f7a8f21e
Matt Burke <matthewcburke@gmail.com>
2012-11-19 16:47:52 -0800

    Successfully calling the getCameraLocation() from Java and returning a float[] from C++. Need to ensure that we have a trackable, and input actual location information. Receiving an IOError from NetworkService when we push the JSON object.

--- vichar/src/edu/pugetsound/vichar/ar/ARGameActivity.java
+++ vichar/src/edu/pugetsound/vichar/ar/ARGameActivity.java
@@ -453,7 +453,7 @@ public class ARGameActivity extends FragmentActivity implements OnTouchListener
      */
     private void onGameStateChange(String stateStr) {
     	
-//    	pushDeviceState(obtainDeviceState());
+    	pushDeviceState(obtainDeviceState());
     	
     	try {
     		JSONObject gameState = new JSONObject(stateStr);
@@ -957,7 +957,7 @@ public class ARGameActivity extends FragmentActivity implements OnTouchListener
     		deviceState.put("rotation", makeRotationJSON(cameraLoc[3], cameraLoc[4], cameraLoc[5]));
     		
     		// Log the position for testing.
-    		DebugLog.LOGI("position: " + cameraLoc[0] + ", " + cameraLoc[1] + ", " + cameraLoc[2]);
+    		DebugLog.LOGI("pushDeviceState::mb" + deviceState.toString());
     		
     		JSONObject sendState = new JSONObject().put(deviceUUID, deviceState);
     		sendState = new JSONObject().put(DEVICES_NAMESPACE, sendState);
